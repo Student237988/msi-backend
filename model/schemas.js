8d8c1748f1c51c4c2d7d4d67128818acab56fbd6
Kacper1104 <kacper.kozimor@gmail.com>
2022-04-05 23:05:18 +0200

    added collections

--- model/schemas.js
+++ model/schemas.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 const User = new mongoose.Schema(
 	{
-		name: String,
-		password: String,
-		email: String,
-		phone: String,
+		id: mongoose.Schema.Types.String,
+		login: mongoose.Schema.Types.String,
+		name: mongoose.Schema.Types.String,
+		password: mongoose.Schema.Types.String,
 		depos: [
 			{
-				depo_id: String,
-				role_id: String
+				depo_id: mongoose.Schema.Types.String,
+				role_id: mongoose.Schema.Types.String
 			}
 		]
 	},
@@ -18,3 +18,36 @@ const User = new mongoose.Schema(
 );
 module.exports.userSchema = User;
 module.exports.users = mongoose.model("user", User, "user");
+
+const Category = new mongoose.Schema({
+	id: mongoose.Schema.Types.String,
+	name: mongoose.Schema.Types.String,
+	comment: mongoose.Schema.Types.String
+});
+module.exports.categorySchema = Category;
+module.exports.categories = mongoose.model("category", Category, "category");
+
+const Depo = new mongoose.Depo({
+	id: mongoose.Schema.Types.String,
+	name: mongoose.Schema.Types.String,
+	email: mongoose.Schema.Types.String,
+	phone: mongoose.Schema.Types.String,
+	address: {
+		city: mongoose.Schema.Types.String,
+		post_code: mongoose.Schema.Types.String,
+		street: mongoose.Schema.Types.String,
+		building: mongoose.Schema.Types.String,
+		premises: mongoose.Schema.Types.String
+	},
+	receipts_diaries: [
+		{
+			diary_id: mongoose.Schema.Types.String,
+			product_id: mongoose.Schema.Types.String,
+			quantity: mongoose.Schema.Types.Number,
+			due_date: mongoose.Schema.Types.Date,
+			comment: mongoose.Schema.Types.String
+		}
+	]
+});
+module.exports.depoSchema = Depo;
+module.exports.depos = mongoose.model("depo", Depo, "depo");
--- routes/user.js
+++ routes/user.js
@@ -6,8 +6,6 @@ module.exports = (app) => {
 	app.get("/users", async (req, res) => {
 		try {
 			let users = await getUsers();
-			// 	.then((res) => console.log("Printing at calling ::", res))
-			// 	.catch((err) => console.log("Err at Calling ::", err));
 			return res.status(200).json(users);
 		} catch (error) {
 			console.log(error);
